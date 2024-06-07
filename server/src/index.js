663dad6fc8739e2d6184a210285a47f7a74d27e9
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-07 18:37:12 +0530

    refactor: indentation

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
-import connectDB from "./db/connect.js";
+import connectDB from "./services/db.js";
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running at port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`server is running at port ${PORT}`);
     });
   })
   .catch((err) => {

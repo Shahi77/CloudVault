4402831876cbcd2bb3d3357ed40bb05ee3eefaeb
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-09 11:44:56 +0530

    feat: add POST /login route and controller

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -29,11 +29,12 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
+  console.log(`entered password: ${password}`);
   return await bcrypt.compare(password, this.password);
 };
 

ddbcd715a369012d6c8eef493c1b408832f7e604
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-09 16:38:05 +0530

    feat: add rate limiting on account creation

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { authLimiter } from "../config/rateLimit.config.js";
 import { handleLogin, handleSignup } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.post("/signup", handleSignup);
+userRouter.post("/signup", authLimiter, handleSignup);
 userRouter.post("/login", handleLogin);
 
 export default userRouter;

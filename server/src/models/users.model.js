ed05c1bb094e826180111bd167dcc2ed9a52e7cb
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-09 11:46:11 +0530

    refactor: change console logs

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -34,7 +34,6 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  console.log(`entered password: ${password}`);
   return await bcrypt.compare(password, this.password);
 };
 

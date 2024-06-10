f3b64f4e6cdd27710328134f613f988728cbd23a
Shahi77 <shahiinfinity02@gmail.com>
2024-06-10 15:12:51 +0530

    adding search and upload label in home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import homePage from "./pages/homePage/homePage";
+import HomePage from "./pages/homePage/homePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<homePage />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- client/src/pages/homePage/homePage.jsx
+++ client/src/pages/homePage/homePage.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./homePage.css";
 
-export default function homePage() {
-  return <></>;
+export default function HomePage() {
+  const [selectedOption, setSelectedOption] = useState("upload");
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div className="radio-inputs">
+      <label className="radio">
+        <input
+          type="radio"
+          name="radio"
+          value="upload"
+          checked={selectedOption === "upload"}
+          onChange={handleOptionChange}
+        />
+        <span className="name">Upload</span>
+      </label>
+      <label className="radio">
+        <input
+          type="radio"
+          name="radio"
+          value="search"
+          checked={selectedOption === "search"}
+          onChange={handleOptionChange}
+        />
+        <span className="name">Search</span>
+      </label>
+    </div>
+  );
 }

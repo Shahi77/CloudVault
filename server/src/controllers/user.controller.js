462fd179c9cf3d1f3df07864d5ed3de715d7126d
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-14 23:36:08 +0530

    fix: update refreshToken error

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -14,7 +14,7 @@ const generateAccessAndRefreshTokens = async (userId) => {
   await User.updateOne(
     { _id: userId },
     {
-      $set: { generatedRefreshToken },
+      $set: { refreshToken: generatedRefreshToken },
     }
   );
 

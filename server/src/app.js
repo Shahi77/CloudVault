be09c0c78275ec674a064b4ce6ed428148612be2
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-30 21:43:11 +0530

    fix: fix CORS error

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,12 @@ import v1Router from "./routes/version1.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));

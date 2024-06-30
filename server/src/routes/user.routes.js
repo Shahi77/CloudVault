8d9dc037b11beef3e953ca6008a7f4359e36acca
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-06-30 16:33:47 +0530

    feat: add check auth controller and router

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -4,12 +4,15 @@ import {
   handleLogin,
   handleRefreshTokens,
   handleSignup,
+  handleCheckAuth,
 } from "../controllers/user.controller.js";
+import verifyToken from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", authLimiter, handleSignup);
 userRouter.post("/login", handleLogin);
 userRouter.post("/refresh-tokens", handleRefreshTokens);
+userRouter.get("/check-auth", verifyToken, handleCheckAuth);
 
 export default userRouter;

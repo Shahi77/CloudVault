554bdc53f51591a602de6ec36aa6a73433efee9a
Shahi77 <shahiinfinity02@gmail.com>
2024-06-30 21:42:12 +0530

    some fixes

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "http://localhost:3000",
+    // origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
